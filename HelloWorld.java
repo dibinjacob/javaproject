825b593e4957ac0fd8282a6b83f0957aac3fded0
dibinjacob <dibinjacob7794@gmail.com>
2021-05-24 23:06:56 +0530

    My first java program

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+            // Print "Hellow world" to the terminal windo.
+System.out.println("Hellow World")
+  }
+ }
+ 
+ 
+ 
+
